76172c4b2a662e0f03358cd232bd28dadb02f8fa
hubi <nicolas.hubacher@epfl.ch>
2015-11-17 22:31:19 +0100

    removed unnecessary files

--- Code/webots/controllers/formation2/build/release/formation2.d
+++
@@ -1,8 +0,0 @@
-build/release/formation2.o: formation2.c \
-  /Applications/Webots/include/controller/c/webots/robot.h \
-  /Applications/Webots/include/controller/c/webots/types.h \
-  /Applications/Webots/include/controller/c/webots/nodes.h \
-  /Applications/Webots/include/controller/c/webots/differential_wheels.h \
-  /Applications/Webots/include/controller/c/webots/distance_sensor.h \
-  /Applications/Webots/include/controller/c/webots/emitter.h \
-  /Applications/Webots/include/controller/c/webots/receiver.h
