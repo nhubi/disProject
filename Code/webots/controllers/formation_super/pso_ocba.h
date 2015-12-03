07257967482073ba13390802085b0eb720a7920e
hubi <nicolas.hubacher@epfl.ch>
2015-12-03 19:52:27 +0100

    Created header file for PSO OCBA

---
+++ Code/webots/controllers/formation_super/pso_ocba.h
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+// This file contains the declarations related to the PSO OCBA algorithm.                         //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef PSO_OCBA_H
+#define PSO_OCBA_H
+
+
+
+// includes
+
+//#include "something.h"
+
+
+
+// definitions
+
+#define NB_ITERATIONS 10
+
+
+
+// declarations
+
+//float something;
+
+
+
+// methods
+
+void pso_ocba();
+
+
+#endif
